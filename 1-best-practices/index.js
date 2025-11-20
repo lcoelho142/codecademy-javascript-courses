361fbf6ecf71a3714d8b01043a9a65e6c946d051
lcoelho142 <lcoelho142@gmail.com>
2025-11-20 17:54:39 -0500

    Best Practices

---
+++ 1-best-practices/index.js
@@ -0,0 +1,11 @@
+// Functional Programming
+// Make everything functions
+// Specify input => Implement functionality => Desiered Output
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 1));
+
+console.log(add(1, 1));
